20706eeddbb961d8b08f0733fc2c8f36b9e4252d
michal <michaelklops200@gmail.com>
2020-06-17 11:31:19 +0200

    fixed

--- src/containers/List/List.js
+++ src/containers/List/List.js
@@ -112,145 +112,156 @@ class List extends Component {
                     colors={['#4b8b1d', '#6cd015']}
                     style={styles.containerColor}
                 />
+                <Header
+                    leftComponent={
+                        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                            <Icon
+                                style={{marginTop: 5, marginLeft: 20}}
+                                size={25} name='arrowleft'
+                                type='antdesign' color={"#fff"}
+                            />
+                        </TouchableOpacity>
+                    }
+                    centerComponent={
+                        <Text style={{
+                            textAlign: 'center',
+                            fontSize: 22,
+                            color: '#fff'
+                        }}>{translations.foodList}</Text>
+                    }
+                    centerSize={6}
+                />
                 {this.state.loading ?
-                    <Spinner color='#000' size={64}/> :
-                    <>
-                        <Header
-                            leftComponent={
-                                <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-                                    <Icon
-                                        style={{marginTop: 5, marginLeft: 20}}
-                                        size={25} name='arrowleft'
-                                        type='antdesign' color={"#fff"}
-                                    />
-                                </TouchableOpacity>
-                            }
-                            centerComponent={
-                                <Text style={{
-                                    textAlign: 'center',
-                                    fontSize: 22,
-                                    color: '#fff'
-                                }}>{translations.foodList}</Text>
-                            }
-                            centerSize={6}
-                        />
-                        <ScrollView>
-                            <View style={styles.container}>
-                                {list.length < 1
-                                    ? <ScrollView>
-                                        : list.map((item, i) => (
-                                        <Swipeable key={i} rightButtons={[
-                                            <>
-                                                <TouchableOpacity onPress={() => this.removeItem(item.id)}
-                                                                  style={styles.delete}>
-                                                    <Icon
-                                                        style={{marginLeft: 10}}
-                                                        size={40} name='trash-o'
-                                                        type='font-awesome' color={'#fff'}/>
-                                                </TouchableOpacity>
-                                                <TouchableOpacity
-                                                    onPress={() => navigation.navigate('Food', {...item})}
-                                                    style={styles.edit}>
-                                                    <Icon
-                                                        style={{marginLeft: 10}}
-                                                        size={40} name='edit'
-                                                        type='font-awesome' color={'#fff'}/>
-                                                </TouchableOpacity>
-                                            </>
-                                        ]}>
-                                            <BlurView style={styles.listItem} intensity={50} tint='dark'>
-                                                <View style={{flex: 1}}>
-                                                    <CheckBox
-                                                        checked={selectedItems.find(i => i === item.id)}
-                                                        onPress={() => this.selectItem(item)}
-                                                        style={styles.checkbox}
-                                                        checkedColor={"#ea6700"}
-                                                        tintColors={{true: '#ea6700', false: '#ea6700'}}
-                                                    />
-                                                </View>
-                                                <View>
-                                                    <Image
-                                                        style={{
-                                                            width: 100,
-                                                            height: 100,
-                                                            marginBottom: 10,
-                                                            resizeMode: item.resizeMode
-                                                        }}
-                                                        source={item.image === 'null' ? require('../../assets/not-found-image.png') : {uri: item.image}}
-                                                    />
-                                                    <ButtonAdd
-                                                        onPressAdd={() => this.addFood(item, 1)}
-                                                        onPresMinus={() => this.addFood(item, -1)}
-                                                        value={item.productQuantity}
-                                                    />
-                                                </View>
-                                                <View style={{flex: 3}}>
-                                                    <Text style={{
-                                                        fontFamily: 'Lato-Bold',
-                                                        fontSize: 20,
-                                                        color: '#fff',
-                                                        marginLeft: 10,
-                                                        marginBottom: 5,
-                                                        marginTop: -30
-                                                    }}>{item.name}</Text>
-                                                    <Text
-                                                        style={styles.text}>{translations.quantity}: {item.quantity}g</Text>
-                                                    <Text
-                                                        style={styles.text}>{translations.percent}: {item.percentage}%</Text>
-                                                </View>
-                                                <Text style={styles.priceText}>{item.price} {currency}</Text>
-                                            </BlurView>
-                                        </Swipeable>
-                                        ))}
-                                    </View>
-                                    < /ScrollView>
-                                    <View style={{
-                                        position: 'absolute',
-                                        width: '100%',
-                                        bottom: 25,
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        shadowColor: "#000",
-                                        shadowOffset: {
-                                            width: 0,
-                                            height: 0
-                                        },
-                                        shadowOpacity: 0.2,
-                                        shadowRadius: 5,
-                                        elevation: 7
-                                    }}>
-                                        <Button
-                                            buttonStyle={{backgroundColor: '#4b8b1d'}}
-                                            disabled={amount === 0}
-                                            titleStyle={{
-                                                color: '#fff',
-                                                fontSize: 18,
-                                                padding: 25,
-                                                fontFamily: 'Lato-Light'
-                                            }}
-                                            title={`${translations.pay} ${amount} ${currency}`}
-                                        />
-                                    </View>
-                                    < />
-                                }
-                            </>
-                            );
-                            }
-                            }
+                    <Spinner bgColor='transparency' color='#000' size={64}/> : <ScrollView>
+                        <View style={styles.container}>
+                            {list.length < 1 ?
+                                <View style={{marginTop: 20}}>
+                                    <Text style={styles.emptyList}>{translations.emptyList}</Text>
+                                    <TouchableOpacity onPress={() => navigation.navigate('Scanner')}>
+                                        <Text style={{
+                                            ...styles.emptyList,
+                                            fontFamily: 'Lato-Regular'
+                                        }}>{translations.scanProduct}</Text>
+                                    </TouchableOpacity>
+                                    <Text style={styles.emptyList}>{translations.or}</Text>
+                                    <TouchableOpacity onPress={() => navigation.navigate('Food')}>
+                                        <Text style={{
+                                            ...styles.emptyList,
+                                            fontFamily: 'Lato-Regular'
+                                        }}>{translations.addManually}</Text>
+                                    </TouchableOpacity>
+                                </View> :
+                                list.map((item, i) => (
+                                    <Swipeable key={i} rightButtons={[
+                                        <>
+                                            <TouchableOpacity onPress={() => this.removeItem(item.id)}
+                                                              style={styles.delete}>
+                                                <Icon
+                                                    style={{marginLeft: 10}}
+                                                    size={40} name='trash-o'
+                                                    type='font-awesome' color={'#fff'}/>
+                                            </TouchableOpacity>
+                                            <TouchableOpacity
+                                                onPress={() => navigation.navigate('Food', {...item})}
+                                                style={styles.edit}>
+                                                <Icon
+                                                    style={{marginLeft: 10}}
+                                                    size={40} name='edit'
+                                                    type='font-awesome' color={'#fff'}/>
+                                            </TouchableOpacity>
+                                        </>
+                                    ]}>
+                                        <BlurView style={styles.listItem} intensity={50} tint='dark'>
+                                            <View style={{flex: 1}}>
+                                                <CheckBox
+                                                    checked={selectedItems.find(i => i === item.id)}
+                                                    onPress={() => this.selectItem(item)}
+                                                    style={styles.checkbox}
+                                                    checkedColor={"#ea6700"}
+                                                    tintColors={{true: '#ea6700', false: '#ea6700'}}
+                                                />
+                                            </View>
+                                            <View>
+                                                <Image
+                                                    style={{
+                                                        width: 100,
+                                                        height: 100,
+                                                        paddingBottom: 10,
+                                                        resizeMode: item.resizeMode
+                                                    }}
+                                                    source={item.image === 'null' ? require('../../assets/not-found-image.png') : {uri: item.image}}
+                                                />
+                                                <ButtonAdd
+                                                    onPressAdd={() => this.addFood(item, 1)}
+                                                    onPresMinus={() => this.addFood(item, -1)}
+                                                    value={item.productQuantity}
+                                                />
+                                            </View>
+                                            <View style={{flex: 3}}>
+                                                <Text style={{
+                                                    fontFamily: 'Lato-Bold',
+                                                    fontSize: 20,
+                                                    color: '#fff',
+                                                    marginLeft: 10,
+                                                    marginBottom: 5,
+                                                    marginTop: -30
+                                                }}>{item.name}</Text>
+                                                <Text
+                                                    style={styles.text}>{translations.quantity}: {item.quantity}g</Text>
+                                                <Text
+                                                    style={styles.text}>{translations.percent}: {item.percentage}%</Text>
+                                            </View>
+                                            <Text style={styles.priceText}>{item.price} {currency}</Text>
+                                        </BlurView>
+                                    </Swipeable>
+                                ))}
+                        </View>
+                    </ScrollView>}
+                <View style={{
+                    position: 'absolute',
+                    width: '100%',
+                    bottom: 25,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    shadowColor: "#000",
+                    shadowOffset: {
+                        width: 0,
+                        height: 0
+                    },
+                    shadowOpacity: 0.2,
+                    shadowRadius: 5,
+                    elevation: 7
+                }}>
+                    <Button
+                        buttonStyle={{backgroundColor: '#4b8b1d'}}
+                        disabled={amount === 0}
+                        titleStyle={{
+                            color: '#fff',
+                            fontSize: 18,
+                            padding: 25,
+                            fontFamily: 'Lato-Light'
+                        }}
+                        title={`${translations.pay} ${amount} ${currency}`}
+                    />
+                </View>
+            </>
+        );
+    }
+}
 
-                            const mapStateToProps = state => {
-                            return {
-                            currency: state.settings.currency,
-                            refresh: state.settings.refresh,
-                            translations: state.settings.translations
-                        }
-                        };
-                            const mapDispatchToProps = dispatch => {
-                            return {
-                            fetchWastedFood: (value) => dispatch(actions.fetchWastedFood(value)),
-                            removeFood: (value) => dispatch(actions.removeFood(value)),
-                            onSaveFood: (value) => dispatch(actions.saveFood(value)),
-                        }
-                        };
+const mapStateToProps = state => {
+    return {
+        currency: state.settings.currency,
+        refresh: state.settings.refresh,
+        translations: state.settings.translations
+    }
+};
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchWastedFood: (value) => dispatch(actions.fetchWastedFood(value)),
+        removeFood: (value) => dispatch(actions.removeFood(value)),
+        onSaveFood: (value) => dispatch(actions.saveFood(value)),
+    }
+};
 
-                            export default connect(mapStateToProps, mapDispatchToProps)(List);
+export default connect(mapStateToProps, mapDispatchToProps)(List);
--- src/router.js
+++ src/router.js
@@ -19,7 +19,7 @@ const MainNavigator = createStackNavigator(
         Settings: {screen: Settings}
     },
     {
-        initialRouteName: 'Home',
+        initialRouteName: 'Food',
         headerMode: 'none'
     },
 );
