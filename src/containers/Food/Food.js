9f1c2f6e367fc686baf71b7df12c8015b51ed9e2
matt <mateuszpijanowski0@gmail.com>
2020-06-17 09:11:44 +0200

    Added version in settings and added manually button in scanner

--- src/containers/Food/Food.js
+++ src/containers/Food/Food.js
@@ -207,7 +207,7 @@ class Food extends Component {
                                     <View style={{
                                         position: 'absolute',
                                         width: '100%',
-                                        bottom: 25,
+                                        bottom: 30,
                                         justifyContent: 'center',
                                         alignItems: 'center',
                                         shadowColor: "#000",
