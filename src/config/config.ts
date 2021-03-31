9b1c0c0169507d96d368f901218897cd6dea7791
Mateusz Pijanowski <mateuszpijanowski0@gmail.com>
2021-03-31 20:41:43 +0200

    Add url to savefood api

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
 export default {
 	FOOD_API: 'https://world.openfoodfacts.org',
 	WS_URL: 'https://webstrong.pl',
+	SAVE_FOOD_API: 'https://webstrong.pl/api/savefood',
 	ASSETS_URL: 'https://webstrong.pl/assets/SaveFood',
 	SETUP_SENTRY: false,
 	SENTRY_DNS: '',
